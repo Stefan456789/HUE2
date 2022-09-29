22faa583977c08abb966e6d03c9919ee67f05f23
Stefan456789 <stefanfarbe@gmail.com>
2022-09-29 09:35:18 +0200

    Added testFile functionality and a file that needs to be tested

--- src/HelloJavamitForEach.java
+++ src/HelloJavamitForEach.java
@@ -1,5 +1,14 @@
-public class Main {
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelloJavamitForEach {
+    public static List<String> list = new ArrayList<>();
     public static void main(String[] args) {
-        
+        for (String s : list) {
+            System.out.println(s);
+        }
+
+        list.forEach(System.out::println);
+
     }
 }
--- src/NumberTester.java
+++ src/NumberTester.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class NumberTester {
+import java.util.Scanner;
+
+public class NumberTester {
+
+    private NumberTest oddTester;
+    private NumberTest primeTester;
+    private NumberTest palindromeTester;
+    private final Scanner s;
+
+    public NumberTester(String filename){
+        s = new Scanner(filename);
+    }
+
+    public void setOddEvenTester(NumberTest oddTester){
+        this.oddTester = oddTester;
+    }
+
+    public void setPrimeTester(NumberTest primeTester){
+        this.primeTester = primeTester;
+    }
+
+    public void setPalindromeTester(NumberTest palindromeTester){
+        this.palindromeTester = palindromeTester;
+    }
+
+    public void testFile(){
+        int lines = Integer.parseInt(s.nextLine());
+        for (int i = 0; i < lines; i++){
+            String[] input = s.nextLine().split("  ");
+            switch (input[0]) {
+                case "1":
+                    System.out.println(oddTester.testNumber(Integer.parseInt(input[1])));
+                    break;
+                case "2":
+                    System.out.println(primeTester.testNumber(Integer.parseInt(input[1])));
+                    break;
+                case "3":
+                    System.out.println(palindromeTester.testNumber(Integer.parseInt(input[1])));
+                    break;
+            }
+        }
+    }
+
 }
+
