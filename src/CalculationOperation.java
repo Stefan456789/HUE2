ebd3b4433e6502b8e191a843a25acf9cb30ddba8
Stefan456789 <stefanfarbe@gmail.com>
2022-09-29 09:42:28 +0200

    Added Getter and Setter for Number Class

--- src/CalculationOperation.java
+++ src/CalculationOperation.java
@@ -1,2 +1,3 @@
-package PACKAGE_NAME;public interface CalculationOperation {
+public interface CalculationOperation {
+    Number calc(Number x, Number y);
 }
--- src/NumberTester.java
+++ src/NumberTester.java
@@ -27,15 +27,25 @@ public class NumberTester {
         int lines = Integer.parseInt(s.nextLine());
         for (int i = 0; i < lines; i++){
             String[] input = s.nextLine().split("  ");
+            boolean result = false;
             switch (input[0]) {
                 case "1":
-                    System.out.println(oddTester.testNumber(Integer.parseInt(input[1])));
+                    result = oddTester.testNumber(Integer.parseInt(input[1]));
+                    if (!result)
+                        System.out.println("NOT ");
+                    System.out.println("EVEN");
                     break;
                 case "2":
-                    System.out.println(primeTester.testNumber(Integer.parseInt(input[1])));
+                    result = primeTester.testNumber(Integer.parseInt(input[1]));
+                    if (!result)
+                        System.out.println("NO ");
+                    System.out.println("PRIME");
                     break;
                 case "3":
-                    System.out.println(palindromeTester.testNumber(Integer.parseInt(input[1])));
+                    result = palindromeTester.testNumber(Integer.parseInt(input[1]));
+                    if (!result)
+                        System.out.println("NO ");
+                    System.out.println("PALINDROME");
                     break;
             }
         }
